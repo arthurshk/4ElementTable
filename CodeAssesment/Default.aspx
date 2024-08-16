b618e3f5e3f87e6cc369c27e4ee6a3f66048c93d
Arthur <arthurdragon39@gmail.com>
2024-08-15 22:47:51 -0400

    Add project files.

---
+++ CodeAssesment/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CodeAssesment._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <p>This is the data from your file</p>
+        <asp:DataGrid ID="myDataGrid" runat="server" AutoGenerateColumns="true" CssClass="datagrid"></asp:DataGrid>
+        
+        <p>These are the creatures broken out by type:</p>
+      <!--create the Data Grids for the filtered "Water" and "Fire" types-->
+        <h3>Fire</h3>
+        <asp:DataGrid ID="fireDataGrid" runat="server" AutoGenerateColumns="true" CssClass="datagrid">
+            <HeaderStyle Font-Bold="true"/>
+        </asp:DataGrid>
+        
+        <h3>Water</h3>
+        <asp:DataGrid ID="waterDataGrid" runat="server" AutoGenerateColumns="true" CssClass="datagrid">
+            <HeaderStyle Font-Bold="true"/>
+        </asp:DataGrid>
+    </div>
+</asp:Content>
---
+++ CodeAssesment/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CodeAssesment
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindDataGrids();
+            }
+        }
+        private void BindDataGrids()
+        {
+            // Map Path to locate and return the csv file to the appropriate path.
+            string filePath = Server.MapPath("~/MyData/DataForTest.csv");
+            DataTable dtData = GetMyData(filePath);
+
+            // Binds the data to the grid with all the data
+            myDataGrid.DataSource = dtData;
+            myDataGrid.DataBind();
+
+            // Filtering data by element types
+            DataTable fireData = FilterDataByType(dtData, "fire");
+            DataTable waterData = FilterDataByType(dtData, "water");
+
+            // Data Binding to their own grids
+            fireDataGrid.DataSource = fireData;
+            fireDataGrid.DataBind();
+
+            waterDataGrid.DataSource = waterData;
+            waterDataGrid.DataBind();
+
+        }
+
+        private DataTable GetMyData(string filePath)
+        {
+            DataTable dtMyDataSet = new DataTable();
+            using (StreamReader fileRead = new StreamReader(filePath))
+            {
+                string line;
+                int row = 0;
+                //Reads the line from the file if it is not null
+                while ((line = fileRead.ReadLine()) != null)
+                {
+                    Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                    string[] x = CSVParser.Split(line);
+                
+                    if (row == 0)
+                    {
+                        foreach (string header in x)
+                        {
+                            dtMyDataSet.Columns.Add(header);
+                        }
+                    }
+                    else
+                    {
+                        DataRow dRow = dtMyDataSet.NewRow();
+                        for (int i = 0; i < x.Length; i++)
+                        {
+                            dRow[i] = x[i];
+                        }
+                        dtMyDataSet.Rows.Add(dRow);
+                    }
+                    row++;
+                }
+            }
+            return dtMyDataSet;
+        }
+
+        private DataTable FilterDataByType(DataTable dataTable, string type)
+        {
+            //Clone the table with only the column names and their data types to set it up
+            DataTable filteredData = dataTable.Clone();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                if (row["Type"].ToString().Equals(type, StringComparison.OrdinalIgnoreCase))
+                {
+                    //Make an import of the entire row when the type matches type passed
+                    filteredData.ImportRow(row);
+                }
+            }
+
+            return filteredData;
+        }
+    }
+}
